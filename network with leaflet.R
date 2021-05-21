5c57c957e058203e6bcbf88e541c3a5dc3e1ae6d
Igor Laltuf <igorlaltuf@gmail.com>
2021-05-21 11:02:52 -0300

    Adicionar cores aos pontos das cidades

--- network with leaflet.R
+++ network with leaflet.R
@@ -25,13 +25,19 @@ mun.inter<- coordMunicipal %>%
   dplyr::filter(code_muni %in% c('1100023','1100122','1100205','1200401','1200203','1302405','1302504','1301902','1304203',
                                  '1304062','1303403','1400472','1400100','1502400','1500602','1501808','1504208','1506138',
                                  '1506807','1600303','1600501','1702109','1709500','1721000','2101202','2109908','2109106',
-                                 '2105302','2103000','5102504','5101803','5107602','5103403','5107909'))
+                                 '2105302','2103000','5102504','5101803','5107602','5103403','5107909')) %>% 
+                  mutate(tipo = c("centroA","centroB","centroA","centroB","centroA","centroB","centroA","centroA","centroA",
+                                  "centroA","centroB","centroA","centroB","centroA","centroB","centroA","centroA","centroA",
+                                  "centroA","centroB","centroA","centroB","centroA","centroB","centroA","centroA","centroA",
+                                  "centroA","centroB","centroA","centroB","centroA","centroB","centroA"))
+
 
 # Organizar dataframe e remover colunas que não serão utilizadas e renomear aquelas que serão usadas.
 coordMunicipal$code_muni <- as.character(coordMunicipal$code_muni)
 mun.inter$code_muni <- as.character(mun.inter$code_muni)
 network$cod_muni_destino <- as.character(network$cod_muni_destino)
 network$cod_muni_origem <- as.character(network$cod_muni_origem)
+
 
 
 network <- merge(network,coordMunicipal,by.x ='cod_muni_origem',by.y = 'code_muni')
@@ -46,13 +52,13 @@ colnames(network)[18] <- 'geom_destino'
 colnames(network)[19] <- 'lat_destino'
 colnames(network)[20] <- 'lng_destino'
 
-network <- network[c(1:4,10:13,19:21)]
+network <- network[c(1:3,9:12,18:20)]
 
 # Criar o mapa
 mapa <- leaflet(shape.amazon) %>% 
   addTiles() %>%  
   addMapPane('polygons',zIndex=410) %>%
-  addMapPane('dots',zIndex=420) #
+  addMapPane('dots',zIndex=420) 
 
 #paleta de cores para categorias de cidades (ex: centroA, centroB)
 pal <- colorFactor(c("red", "navy"), domain = c("centroA", "centroB"))
@@ -68,9 +74,8 @@ mapa <- mapa %>%
     data = mun.inter,
     lat = ~lat,
     lng = ~lng,
-    radius = 5,
-    #radius = ~ifelse(tipo == "centroA", 5, 4), ########
-    #color = ~pal(tipo), # Aqui ele busca na variável pal o domain da linha e retorna a cor do color factor
+    radius = ~ifelse('tipo' == "centroA", 5, 4), 
+    color = ~pal(tipo), # Aqui ele busca na variável pal o domain da linha e retorna a cor do color factor
     stroke = FALSE, 
     fillOpacity = 0.3,
     label = mun.inter$name_muni,
@@ -89,10 +94,10 @@ repeat{
   m <- network$lng_destino[i]
   
   mapa <- mapa %>% 
-            addPolylines(data = network, #aqui as polylines estão seguindo a ordem das linhas no arquivo. Criar um arquivo novo?
-                         lng = c(l,m), #~lng, 
-                         lat = c(k,j), #~lat,
-                         weight = .7,
+            addPolylines(data = network, 
+                         lng = c(l,m),  
+                         lat = c(k,j), 
+                         weight = .9,
                          labelOptions = labelOptions(
                            options = pathOptions(pane = 'dots')
                          ))
